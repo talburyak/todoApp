899c1ef0befdb054cc5af838d12df6334358554d
talburyak <talburyak@gmail.com>
2020-08-20 12:32:00 +0300

    :commit sql

--- todoItemsApp/Dump20200820.sql
+++ todoItemsApp/Dump20200820.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `todoapp_schema` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-USE `todoapp_schema`;
 -- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: todoapp_schema
@@ -16,6 +14,10 @@ USE `todoapp_schema`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping routines for database 'todoapp_schema'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -26,4 +28,4 @@ USE `todoapp_schema`;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-20 11:30:21
+-- Dump completed on 2020-08-20 12:29:49
