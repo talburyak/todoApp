b7ee40b4bd9f42ec11831b93a6ed0bd2ecb16c6c
talburyak <talburyak@gmail.com>
2020-09-02 17:21:22 +0300

    component if page not founded

--- src/app/notFoundComponent.component.ts
+++ src/app/notFoundComponent.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'notFoundComponent',
-  template: '<h1>not Found Page!</h1> ',
+  template: '<h1>Not Found Page!</h1> ',
 })
 export class NotFoundComponent {}
